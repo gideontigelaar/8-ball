e79bbc3762c84ea7a71e8795f5f16b5af3c10532
KoningWoefie <sander@hdms.nl>
2026-03-20 09:24:11 +0100

    adding visuals

--- app/static/js/game.js
+++ app/static/js/game.js
@@ -2,6 +2,9 @@ let cnv;
 let aspectRatio = 16/9;
 const margin = convertRemToPixels(8);
 let container = document.getElementById("main");
+let balls = []
+let cue;
+let whiteBall;
 
 function setup() 
 {
@@ -11,23 +14,49 @@ function setup()
 
     container.width = (windowHeight - margin) * aspectRatio;
     container.height = windowHeight - margin;
+
+    whiteBall = createVector(300, container.height/2, 60);
+
+    // Balls
+    for (let i = 0; i < 5; i += 1)
+    {
+        for(let j = 0; j <= i; j += 1)
+        {
+            balls.push(createVector(1000 + 55*i, container.height/2 + 31 * i - 62 * j, 60));
+        }
+    }
 }
 
 function draw() 
 {
-    background(220);
-    //when mouse button is pressed, circles turn black
-    if (mouseIsPressed === true) 
-    {
-        fill(0);
-    } 
-    else 
-    {
-        fill(255);
-    }
+    background("#36594A");
 
-    //white circles drawn at mouse position
-    circle(mouseX, mouseY, 100);
+    
+    // Walls
+    noStroke();    
+    fill("#895129");
+    rect(0, 0, 40, container.height);
+    rect(container.width - 40, 0, 40, container.height);
+    rect(0, 0, container.width, 40);
+    rect(0, container.height - 40, container.width, 40);
+    
+    // Corners
+    fill(0);
+    circle(60,60,80)
+    circle(container.width/2,60,80)
+    circle(60,container.height - 60,80)
+    circle(container.width/2,container.height - 60,80)
+    circle(container.width-60, 60, 80)
+    circle(container.width-60, container.height - 60, 80)
+
+    // White ball
+    fill(255);
+    circle(whiteBall.x, whiteBall.y, whiteBall.z);
+    
+    for (let i = 0; i < balls.length; i += 1)
+    {
+        circle(balls[i].x, balls[i].y, balls[i].z)
+    }
 }
 
 function windowResized() 
