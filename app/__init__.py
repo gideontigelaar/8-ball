516afd22ccc16b7a0234d23b17c4793e2200ba1f
Gideon Tigelaar <contact@gideontigelaar.com>
2026-03-20 12:16:03 +0100

    Add basic creds handling for login

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,33 @@
 from flask import Flask
+from flask_login import LoginManager
+import pymysql
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
+app.secret_key = os.environ["SECRET_KEY"]
 
-# Import routes after app is created to avoid circular imports
-from app.api import routes
+DB_CONFIG = {
+    "host": os.environ["DB_HOST"],
+    "user": os.environ["DB_USER"],
+    "password": os.environ["DB_PASSWORD"],
+    "db": os.environ["DB_NAME"],
+    "charset": "utf8mb4",
+    "cursorclass": pymysql.cursors.DictCursor,
+}
+
+def get_db():
+    return pymysql.connect(**DB_CONFIG)
+
+login_manager = LoginManager(app)
+login_manager.login_view = "login"
+
+from app.models import User
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get_by_id(int(user_id))
+
+from app import routes
